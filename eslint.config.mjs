894c33798b44451f4def87fd7eff00711c3a7624
Indranil Halder <indranilhalder.dev@gmail.com>
2025-08-16 12:32:25 +0530

    Initial app setup
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginReact from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import typescriptParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +17,42 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { files: ["**/*.{js,ts,jsx,tsx}"] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser: typescriptParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  eslintConfigPrettier,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  pluginReact.configs.flat.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+  },
+  {
+    ignores: [
+      ".next/",
+      "node_modules",
+      "coverage",
+      "build",
+      "*.config.mjs",
+      "convex",
+      "src/db/prisma/**",
+    ],
+  },
 ];
 
 export default eslintConfig;
